d4fb768c0d929c28200edc6ab6305e79a66bf99a
mahsahajirahimi <mahsahajirahimi2003@gmail.com>
2024-12-13 02:07:59 -0800

    final changes on auth page

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -26,25 +26,25 @@ router.post('/login', (req, res) => {
 
     const user = userList.find(user => user.username === username);
     if (!user) {
-        return res.status(404).send('User not found. Please register.');
+        return res.status(404).send('نام کاربری یافت نشد.لطفا ثبت نام کنید.');
     }
     if (user.password !== password) {
-        return res.status(401).send('Incorrect password.');
+        return res.status(401).send('گذرواژه نادرست است');
     }
-    res.status(200).send('Login successful');
+    res.status(200).send('ورود موفقیت آمیز بود');
 });
 
 // Register route for players and designers
 router.post('/register', (req, res) => {
-    const { role, username, password, name, email, ...rest } = req.body;
+    const { role, username, password, name, email, gender } = req.body;
     const userList = role === 'player' ? players.players : tarrahs.tarrahs;
 
     const userExists = userList.find(user => user.username === username || user.email === email);
     if (userExists) {
-        return res.status(409).send('Username or email already exists. Please login.');
+        return res.status(409).send('نام کاربری/ایمیل تکراری است. لطفا وارد شوید');
     }
 
-    const newUser = { username, password, name, email, ...rest };
+    const newUser = { username, password, name, email, gender };
     userList.push(newUser);
 
     // Save data back to the JSON file
@@ -54,7 +54,7 @@ router.post('/register', (req, res) => {
         saveJSONFile(tarrahsFilePath, { tarrahs: userList });
     }
 
-    res.status(201).send('User registered successfully');
+    res.status(201).send('ثبت نام موفقیت آمیز بود');
 });
 
 module.exports = router;
