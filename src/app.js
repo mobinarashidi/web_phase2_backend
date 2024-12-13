d7255ca8126d9b8845be4fd2120b1c7b9cb0451e
mahsahajirahimi <mahsahajirahimi2003@gmail.com>
2024-12-13 07:06:30 -0800

    tarrah.jaon updated and fetures on profiles added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
-
 const categoriesRoutes = require('./routes/categories');
 const questionsRoutes = require('./routes/questions');
 const answeredQuestionsRoutes = require('./routes/answeredQuestions');
-const profilesRoutes = require('./routes/profiles');
+const profilesRoutes = require('./routes/profiles'); // اضافه کردن مسیر پروفایل‌ها
 const playerProfileCardRoutes = require('./routes/playerProfileCard');
 const tarrahsRoutes = require('./routes/tarrahs');
+const answeringRoutes = require('./routes/answering');
 
 const app = express();
 
@@ -20,13 +20,16 @@ app.use('/api/auth', authRoutes);
 app.use('/api/categories', categoriesRoutes);
 app.use('/api/questions', questionsRoutes);
 app.use('/api/answered-questions', answeredQuestionsRoutes);
-app.use('/api/profiles', profilesRoutes);
+app.use('/api/profiles', profilesRoutes); // اضافه کردن مسیر پروفایل‌ها
 app.use('/api/player', playerProfileCardRoutes);
 app.use('/api/tarrahs', tarrahsRoutes);
+app.use('/api/answering', answeringRoutes);
 
 // Default route
 app.get('/', (req, res) => {
     res.send('Welcome to the Quiz API!');
 });
+
+
 
 module.exports = app;
